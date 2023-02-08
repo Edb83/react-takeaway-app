e14e3e31195b4d2809d8f4ec31cf9ed4c1aa4e4c
Ed Bradley <ed3radley@gmail.com>
2023-02-08 11:02:22 +0000

    Add HeaderCardButton and CartIcon components

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import styles from './Header.module.css';
+import HeaderCardButton from './HeaderCardButton';
 
 const Header = (props) => {
   return (
     <React.Fragment>
         <header className={styles.header}>
             <h1>ReactMeals</h1>
-            <button>Cart</button>
+            <HeaderCardButton/>
         </header>
         <div className={styles['main-image']}>
             <img src={mealsImage} alt='A table full of food' />
