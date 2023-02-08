648ac395e04ffa739bdd2ffa522eefc8dca6469d
Ed Bradley <ed3radley@gmail.com>
2023-02-08 15:44:45 +0000

    Add MealItem, MealItemForm, Cart, Card and Input components

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import MealItemForm from './MealItemForm';
+import styles from './MealItem.module.css';
+
+const MealItem = (props) => {
+    const price = `£${props.price.toFixed(2)}`;
+    return (
+        <li className={styles.meal}>
+            <div>
+                <h3>{props.name}</h3>
+                <div className={styles.description}>{props.description}</div>
+                <div className={styles.price}>{price}</div>
+            </div>
+            <MealItemForm id={props.id} />
+        </li>
+    )
+}
+
+export default MealItem
---
+++ src/components/UI/Input.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+const Input = (props) => {
+  return (
+    <div className={styles.input}>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input
+        // spread operator used to get key, value pairs of props from 'input' object
+            {...props.input}
+        />
+    </div>
+  )
+}
+
+export default Input
