857d3a67f3155c45b708ffdca346346b0d19e54e
Ed Bradley <ed3radley@gmail.com>
2023-03-02 17:49:16 +0000

    Add useEffect to play animation by adding bump class to cart icon, inc cleanup function

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,17 +1,36 @@
-import React, { useContext} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import styles from './HeaderCardButton.module.css';
 
 const HeaderCardButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+  const {items} = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((curr, item) => {
+  const numberOfCartItems = items.reduce((curr, item) => {
     return curr + item.amount;
   }, 0)
 
+  const btnStyles = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300)
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items])
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnStyles} onClick={props.onClick}>
         <span className={styles.icon}>
             <CartIcon />
         </span>
