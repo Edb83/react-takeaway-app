5f5466849598fe59ed42e3bcbbb8c368ca3b67ca
Ed Bradley <ed3radley@gmail.com>
2023-03-02 17:24:37 +0000

    Add CartItem component and update CartProvider to aggregate items added to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,22 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
-    const cartItems = <ul className={styles['cart-items']}>
-        {[
-            { id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }
-        ].map(item => <li>{item.name}</li>)}</ul>;
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`
+    const hasItems = cartCtx.items.length > 0;
+
+    const cartItemAddHandler = item => {
+
+    }
+
+    const cartItemRemoveHandler = id => {
+
+    }
+
+
+    const cartItems = (
+        <ul className={styles['cart-items']}>
+            {cartCtx.items.map(item => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                />
+            ))}
+        </ul>
+    );
     return (
         <Modal onCloseCart={props.onCloseCart}>
             {cartItems}
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={styles.actions}>
                 <button className={styles['button--alt']} onClick={props.onCloseCart}>Close</button>
-                <button className={styles.button}>Order</button>
+                {hasItems && <button className={styles.button}>Order</button>}
             </div>
         </Modal>
     )
